86f323b65fcbe83f5deba8937f7916d65c4b8686
francisconavy <francisconavy@yahoo.com.br>
2018-06-17 13:32:59 -0300

    Arrumando programa para respostas

--- prog.py
+++ prog.py
@@ -1,11 +1,21 @@
 import random
 arq = open("perguntas.txt",'r')
+arq2 = open("respostas.txt", 'r')
 perg = arq.readline()
-data_base = list()
+resp = arq2.readline()
+data_base_p = list()
+data_base_r = list()
 while perg != "fim\n":
-	data_base.append(perg)
-	perg = arq.readline()
+    data_base_p.append(perg)
+    data_base_r.append(resp)
+    perg = arq.readline()
+    resp = arq2.readline()
+
+arq.close()
+arq2.close()
 prox = input("Digite qualquer coisa para iniciar, e 'sair' quando acabar")
 while prox != "sair":
-	print(data_base[random.randint(0, len(data_base)-1)])
-	prox = input()
+    aleatorio = random.randint(0, len(data_base_p)-1)
+    print(data_base_p[aleatorio], end="")
+    prox = input()
+    print("Resposta:", data_base_r[aleatorio])
